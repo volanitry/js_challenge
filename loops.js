3ca825de79e99843ae369e096364a1332dbbc5d8
= <=>
2023-09-30 13:01:42 +0100

    my first commit

---
+++ loops.js
@@ -0,0 +1,30 @@
+
+let today = "saturday"
+
+if (today == "saturday") {
+    console.log ("Come to the coding school")
+
+}else if (today == "sunday") {
+    console.log ("sleep")
+}else if (today == "monday") {
+    console.log ("go to school")
+}else {
+    console.log ("do whatever you want")
+}
+
+switch (today) {
+    case"saturday":
+    console.log("Coding time...")
+    break;
+    case"sunday":
+    console.log("Sleeping time...")
+    break;
+    case"monday":
+    console.log("School time...")
+    break;
+    
+    default:
+        console.log("free time")
+        break;
+}
+
