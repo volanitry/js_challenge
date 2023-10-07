bb068478354dc55f97cc54d053357a71fef7f553
= <=>
2023-10-07 13:18:16 +0100

    added funtion code

---
+++ funtions.js
@@ -0,0 +1,9 @@
+// write a simple js funtion called multiply that takes two members as params and returns to the caller
+
+function multiply(num1, num2) {
+    return (num1 * num2);
+}
+
+
+let result = multiply (5,6)
+console.log(result)
