cdd4f13ae7a24b0b10412bec65abc4b0b4bc26d7
= <=>
2023-10-07 13:40:55 +0100

    i added and fuctions

--- funtions.js
+++ funtions.js
@@ -1,8 +1,18 @@
 // write a simple js funtion called multiply that takes two members as params and returns to the caller
 
-function multiply(num1, num2) {
-    return (num1 * num2);
-}
+// function multiply(num1, num2) {
+//     return (num1 * num2);
+// }
+
+
+// let result = multiply (5,6)
+// console.log(result)
+
+// convert the multiply funtion  above to an arrow function
+
+
+let multiply =  (num1, num2) => num1 * num2
+    
 
 
 let result = multiply (5,6)
