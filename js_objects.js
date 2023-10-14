e2852bca6bb79253e0d81530dfd7422f6d3f42a4
= <=>
2023-10-14 13:17:12 +0100

    added objects

--- js_objects.js
+++ js_objects.js
@@ -1,1 +1,12 @@
- 
+let player1 = {
+    firstName : "Cristiano",
+    lastName: "Ronaldo",
+    age : 38,
+    hasKids : true,
+
+    shoot : function () {
+        console.log( this.lastName + " shot the ball")
+    }
+}
+
+console.log(player1.shoot())
